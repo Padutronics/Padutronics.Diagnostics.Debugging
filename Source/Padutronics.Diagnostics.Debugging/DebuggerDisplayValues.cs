1ed9dd4515904d264069ccdc3ce3cc2760991fa6
Pavel Dubsky <pavel.dubsky@padutronics.com>
2022-11-07 01:10:00 +0200

    Add common values for System.Diagnostics.DebuggerDisplay attribute

---
+++ Source/Padutronics.Diagnostics.Debugging/DebuggerDisplayValues.cs
@@ -0,0 +1,8 @@
+namespace Padutronics.Diagnostics.Debugging;
+
+public static class DebuggerDisplayValues
+{
+    public const string DebuggerDisplay = "{DebuggerDisplay,nq}";
+    public const string ToStringWithQuotes = "{ToString()}";
+    public const string ToStringWithoutQuotes = "{ToString(),nq}";
+}
